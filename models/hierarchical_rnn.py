e2220cc7fea0cd10b7ab0e12e800bb4e69a6ef4c
Potsawee Manakul <m.potsawee@gmail.com>
2019-12-17 15:36:48 +0000

    fix beamsearch decode issue

--- models/hierarchical_rnn.py
+++ models/hierarchical_rnn.py
@@ -64,6 +64,7 @@ class EncoderDecoder(nn.Module):
                 - keypadmask_dtype = torch.bool
         """
         k                = decode_dict['k']
+        search_method    = decode_dict['search_method']
         batch_size       = decode_dict['batch_size']
         time_step        = decode_dict['time_step']
         vocab_size       = decode_dict['vocab_size']
@@ -89,13 +90,17 @@ class EncoderDecoder(nn.Module):
 
         finished_beams = [[] for _ in range(batch_size)]
 
+        beam_ht = [self.decoder.init_h0(batch_size) for _ in range(k)]
+        finish = False
         for t in range(time_step-1):
+            if finish: break
             decoder_output_t_array = torch.zeros((batch_size, k*vocab_size))
 
             for i, beam in enumerate(beams):
 
                 # inference decoding
-                decoder_output = self.decoder(beam[:,:t+1], s_output, s_len, logsoftmax=True)[:,-1,:]
+                # decoder_output = self.decoder(beam[:,:t+1], s_output, s_len, logsoftmax=True)[:,-1,:]
+                decoder_output, beam_ht[i] = self.decoder.forward_step(beam[:,t:t+1], beam_ht[i], s_output, s_len, logsoftmax=True)
 
                 # check if there is STOP_TOKEN emitted in the previous time step already
                 # i.e. if the input at this time step is STOP_TOKEN
@@ -110,19 +115,29 @@ class EncoderDecoder(nn.Module):
                 for n_idx in range(batch_size):
                     decoder_output_t_array[n_idx,i*vocab_size:(i+1)*vocab_size] += beam_scores[n_idx,i]
 
+                # only support batch_size = 1!
+                if t == 0:
+                    decoder_output_t_array[n_idx,(i+1)*vocab_size:] = float('-inf')
+                    break
 
-            # scores, indices = torch.topk(decoder_output_t_array, k=k, dim=-1)
-            scores  = np.zeros((batch_size, k))
-            indices = np.zeros((batch_size, k))
-            # pmf = nn.functional.softmax(decoder_output_t_array, dim=-1).cpu().numpy()
-            pmf = np.exp(decoder_output_t_array.cpu().numpy())
-            for bi in range(batch_size):
-                if pmf[bi].sum() != 1.0:
-                    pmf[bi] /= pmf[bi].sum()
-                sampled_ids = np.random.choice(k*vocab_size, size=k, p=pmf[bi])
-                for _s, s_id in enumerate(sampled_ids):
-                    scores[bi, _s]  = decoder_output_t_array[bi, s_id]
-                    indices[bi, _s] = s_id
+            if search_method == 'sampling':
+                # Sampling
+                scores  = np.zeros((batch_size, k))
+                indices = np.zeros((batch_size, k))
+                pmf = np.exp(decoder_output_t_array.cpu().numpy())
+                for bi in range(batch_size):
+                    if pmf[bi].sum() != 1.0:
+                        pmf[bi] /= pmf[bi].sum()
+                    sampled_ids = np.random.choice(k*vocab_size, size=k, p=pmf[bi])
+                    for _s, s_id in enumerate(sampled_ids):
+                        scores[bi, _s]  = decoder_output_t_array[bi, s_id]
+                        indices[bi, _s] = s_id
+
+            elif search_method == 'argmax':
+                # Argmax
+                topk_scores, topk_ids = torch.topk(decoder_output_t_array, k, dim=-1)
+                scores = topk_scores.double().cpu().numpy()
+                indices = topk_ids.double().cpu().numpy()
 
             new_beams = [torch.zeros((batch_size, time_step), dtype=torch.int64).to(device) for _ in range(k)]
             for r_idx, row in enumerate(indices):
@@ -138,24 +153,36 @@ class EncoderDecoder(nn.Module):
                         finished_beams[r_idx].append(new_beams[c_idx][r_idx,:t+1+1])
                         scores[r_idx, c_idx] = float('-inf')
 
-            # normalisation the score
-            scores = np.exp(scores)
-            scores = scores / scores.sum(axis=-1)
-            beam_scores = np.log(scores + 1e-20) # suppress warning log(zero)
+            # only support BATCH SIZE = 1
+            count_stop = 0
+            for ik in range(k):
+                if scores[0,ik] == float('-inf'): count_stop += 1
+            if count_stop == k: finish = True
+
             beams = new_beams
+            if search_method == 'sampling':
+                # normalisation the score
+                scores = np.exp(scores)
+                scores = scores / scores.sum(axis=-1).reshape(batch_size, 1)
+                beam_scores = np.log(scores + 1e-20) # suppress warning log(zero)
+            elif search_method == 'argmax':
+                beam_scores = scores
+
+
 
             # print("=========================  t = {} =========================".format(t))
-            # print("beam1: [{:.5f}]".format(scores[0,0]),bert_tokenizer.decode(beams[0][0].cpu().numpy()[:t+1]))
-            # print("beam2: [{:.5f}]".format(scores[0,1]),bert_tokenizer.decode(beams[1][0].cpu().numpy()[:t+1]))
-            # print("beam3: [{:.5f}]".format(scores[0,2]),bert_tokenizer.decode(beams[2][0].cpu().numpy()[:t+1]))
-            # print("beam4: [{:.5f}]".format(scores[0,3]),bert_tokenizer.decode(beams[3][0].cpu().numpy()[:t+1]))
-            # print("beam5: [{:.5f}]".format(scores[0,4]),bert_tokenizer.decode(beams[4][0].cpu().numpy()[:t+1]))
+            # for ik in range(k):
+            #     print("beam{}: [{:.5f}]".format(ik, scores[0,ik]),bert_tokenizer.decode(beams[ik][0].cpu().numpy()[:t+2]))
             # pdb.set_trace()
 
             if (t % 50) == 0:
                 print("{}=".format(t), end="")
                 sys.stdout.flush()
         print("{}=#".format(t))
+
+        for bi in range(batch_size):
+            if len(finished_beams[bi]) == 0:
+                finished_beams[bi].append(beams[0][bi])
 
         summaries_id = [None for _ in range(batch_size)]
         # for j in range(batch_size): summaries_id[j] = beams[0][j].cpu().numpy()
@@ -179,8 +206,9 @@ class EncoderDecoder(nn.Module):
                 pred_t  = seq[t+1]
                 score_t = decoder_output[0, t, pred_t]
                 score  += score_t
-
             scores[i] = score.cpu().numpy().item() / (timesteps)**alpha
+            # print("SCORING: beam{}: score={:2f} --- len={} --- norm_score={}".format(i,score.cpu().numpy().item(), timesteps, scores[i]))
+            # pdb.set_trace()
         return scores
 
     def decode_sampling(self, input, u_len, w_len, decode_dict):
@@ -220,6 +248,7 @@ class EncoderDecoder(nn.Module):
         for t in range(time_step-1):
 
             decoder_output = self.decoder(tgt_ids[:,:t+1], s_output, s_len, logsoftmax=False)[:,-1,:]
+
             pmf = nn.functional.softmax(decoder_output, dim=-1).cpu().numpy()
             for bn in range(batch_size):
                 id  = np.random.choice(vocab_size, p=pmf[bn])
@@ -231,13 +260,12 @@ class EncoderDecoder(nn.Module):
                 sys.stdout.flush()
 
         print("{}=#".format(t))
-        # print(bert_tokenizer.decode(tgt_ids[0].cpu().numpy()))
+        print(bert_tokenizer.decode(tgt_ids[0].cpu().numpy()))
 
         summaries_id = [None for _ in range(batch_size)]
         for j in range(batch_size): summaries_id[j] = tgt_ids[j].cpu().numpy()
 
         return summaries_id
-
 
 
 class HierarchicalGRU(nn.Module):
@@ -275,6 +303,7 @@ class HierarchicalGRU(nn.Module):
         embed = embed.view(batch_size*num_utterances, num_words, self.embedding_dim)
 
         # word-level GRU
+        self.gru_wlevel.flatten_parameters()
         w_output, _ = self.gru_wlevel(embed)
         w_len = w_len.reshape(-1)
 
@@ -296,6 +325,7 @@ class HierarchicalGRU(nn.Module):
             for s_idx, s_pos in enumerate(segment_indices[bn]):
                 seg_input[bn, s_idx] = utt_output[bn, s_pos]
 
+        self.gru_slevel.flatten_parameters()
         s_output, _ = self.gru_slevel(seg_input)
 
         return s_output, s_len
@@ -313,16 +343,20 @@ class DecoderGRU(nn.Module):
 
         self.embedding = nn.Embedding(vocab_size, embedding_dim=embedding_dim, padding_idx=0)
 
+        # self.rnn = nn.GRU(embedding_dim, dec_hidden_size, num_layers, batch_first=True, dropout=dropout)
         for n in range(num_layers):
             if n == 0:
                 setattr(self, 'rnn_layer_{}'.format(n), nn.GRU(embedding_dim, dec_hidden_size, 1, batch_first=True, dropout=0.0))
             else:
                 setattr(self, 'rnn_layer_{}'.format(n), nn.GRU(dec_hidden_size, dec_hidden_size, 1, batch_first=True, dropout=0.0))
 
-        # self.rnn = nn.GRU(embedding_dim, dec_hidden_size, num_layers, batch_first=True, dropout=dropout)
         self.dropout_layer = nn.Dropout(p=dropout)
 
         self.attention = nn.Linear(mem_hidden_size, dec_hidden_size)
+        ### TODO: attention mechanism & coverage !!!
+        # self.attn_W = nn.Linear(mem_hidden_size+dec_hidden_size, dec_hidden_size, bias=True)
+        # self.attn_v = nn.Linear(dec_hidden_size, 1, bias=False)
+
         self.attn_softmax = nn.Softmax(dim=-1)
         self.output_layer = nn.Linear(embedding_dim+dec_hidden_size+mem_hidden_size, vocab_size, bias=True)
         self.logsoftmax = nn.LogSoftmax(dim=-1)
@@ -334,11 +368,14 @@ class DecoderGRU(nn.Module):
             rnn_layer_n = getattr(self, 'rnn_layer_{}'.format(n))
             if n == 0:
                 _x = embed
+                rnn_layer_n.flatten_parameters()
                 rnn_output, _ = rnn_layer_n(_x)
+                ### DEC10A ###
+                # _x = torch.cat((_x,_x), -1)
             else:
                 _x = self.dropout_layer(rnn_output) + _x # residual connection
+                rnn_layer_n.flatten_parameters()
                 rnn_output, _ = rnn_layer_n(_x)
-
 
         # attention mechanism
         scores = torch.bmm(rnn_output, self.attention(enc_output).permute(0,2,1))
@@ -355,16 +392,40 @@ class DecoderGRU(nn.Module):
 
         return dec_output
 
-    # def forward_step(self, xt, ht, enc_output, enc_output_len):
-    #     xt = self.embedding(xt) # xt => [batch_size, 1, input_size]
-    #                             # ht => [batch_size, num_layers, hidden_size]
-    #     ht1 = torch.zeros((xt.size(0)))
-    #     for n in range(self.num_layers):
-    #         rnn_layer_n = getattr(self, 'rnn_layer_{}'.format(n))
-    #         _h = ht[:,n,:]
-    #         if n == 0:
-    #             _x = embed
-    #             _, ht1 = rnn_layer_n(_x, _h)
-    #         else:
-    #             _x = self.dropout_layer(rnn_output) + _x # residual connection
-    #             rnn_output, _ = rnn_layer_n(_x)
+    def forward_step(self, xt, ht, enc_output, enc_output_len, logsoftmax=True):
+        xt = self.embedding(xt) # xt => [batch_size, 1, input_size]
+                                # ht => [batch_size, num_layers, hidden_size]
+        ht1 = torch.zeros((ht.size(0), ht.size(1), ht.size(2))).cuda()
+        for n in range(self.num_layers):
+            rnn_layer_n = getattr(self, 'rnn_layer_{}'.format(n))
+            _h = ht[:,n:n+1,:]
+            if n == 0:
+                _x = xt
+                _, _h1 = rnn_layer_n(_x, _h) # h1 => [batch_size, 1, hidden_size]
+
+            else:
+                _x = self.dropout_layer(_h1) + _x # residual connection
+                _, _h1 = rnn_layer_n(_x, _h) # h1 => [batch_size, 1, hidden_size]
+
+            ht1[:,n:n+1,:] = _h1
+
+        rnn_output = _h1
+
+        # attention mechanism
+        scores = torch.bmm(rnn_output, self.attention(enc_output).permute(0,2,1))
+        # add bias -inf to the padded positions
+        for bn, l in enumerate(enc_output_len):
+            scores[bn,:,l:].fill_(float('-inf'))
+        scores = self.attn_softmax(scores)
+        context_vec = torch.bmm(scores, enc_output)
+
+        dec_output = self.output_layer(torch.cat((context_vec, rnn_output, xt), dim=-1))
+
+        if logsoftmax:
+            dec_output = self.logsoftmax(dec_output)
+
+        return dec_output[:,-1,:], ht1
+
+    def init_h0(self, batch_size):
+        h0 = torch.zeros((batch_size, self.num_layers, self.dec_hidden_size)).cuda()
+        return h0
